d7260dfd18f07fb6b3e5af7522ad75106de41c1c
Achyut1986 <82679069+Achyut1986@users.noreply.github.com>
2021-05-01 13:24:16 -0500

    added depoit funds

--- src/main/java/com/cognizant/Lender.java
+++ src/main/java/com/cognizant/Lender.java
@@ -1,7 +1,16 @@
 package com.cognizant;
 
 public class Lender {
+
+    int availableFunds;
+
     public int checkAvailableFunds() {
-        return 0;
+        return availableFunds;
+    }
+
+    public void depositFunds(int amount) {
+
+        availableFunds = availableFunds + amount;
+
     }
 }
--- src/test/java/com/cognizant/MortgageLendertest.java
+++ src/test/java/com/cognizant/MortgageLendertest.java
@@ -18,12 +18,35 @@ public class MortgageLendertest {
         //SETUP
         Lender lender = new Lender();
 
+        //ASSERTION
+        assertEquals(0,lender.checkAvailableFunds());
+    }
+    @Test
+    void depositFunds() {
+
+       // As a lender, I want to add money to my available funds,
+        // so that I can offer loans to potential home buyers.
+//
+//        Given I have <current_amount> available funds
+//        When I add <deposit_amount>
+//        Then my available funds should be <total>
+//
+//                Examples:
+//| current_amount | deposit_amount |   total  |
+//|     100,000    |      50,000    | 150,000  |
+//|     200,000    |      30,000    | 230,000  |
+
+
+        //SETUP
+        Lender lender = new Lender();
+
         //EXECUTION
-        int actual = lender.checkAvailableFunds();
-        int expected = 0;
+        lender.depositFunds(100000);
+        lender.depositFunds(1);
 
         //ASSERTION
-        assertEquals(expected,actual);
+        assertEquals(100001,lender.checkAvailableFunds());
+
     }
 
 }
