fb9f52c9eb459f661b5907b950de8128e25356b9
Temitope Awodiji <80483551+TEMI-AWODIJI@users.noreply.github.com>
2021-05-01 11:22:34 -0500

    added checkAvailableFunds

---
+++ src/main/java/com/cognizant/Lender.java
@@ -0,0 +1,7 @@
+package com.cognizant;
+
+public class Lender {
+    public int checkAvailableFunds() {
+        return 0;
+    }
+}
--- src/test/java/com/cognizant/MortgageLendertest.java
+++ src/test/java/com/cognizant/MortgageLendertest.java
@@ -1,5 +1,30 @@
 package com.cognizant;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MortgageLendertest {
+
+    //As a lender, I want to be able to check my available funds, so that I know how much money I can offer for loans.
+
+   // When I check my available funds
+   // Then I should see how much funds I currently have
+
     @Test
+    void checkAvailableFunds() {
+
+        //SETUP
+        Lender lender = new Lender();
+
+        //EXECUTION
+        int actual = lender.checkAvailableFunds();
+        int expected = 0;
+
+        //ASSERTION
+        assertEquals(expected,actual);
+    }
+
 }
+
